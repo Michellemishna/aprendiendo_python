ff1a28d21044b2c5fd14b4e2813e038ff5c134b2
Michelle Diaz <michellelunaby@gmail.com>
2023-09-06 01:52:08 -0600

    input numeros

---
+++ inputs/input_numeros.py
@@ -0,0 +1,10 @@
+#pedir dato al usuario
+numero = input("Me puedes dar un numero: ")
+
+#multiplicando por 2
+#resultado = int(numero)*2
+#convertir a float el resultado (es decir con decimal)
+
+resultado = float(numero)*2
+#mostrar dato
+print(f'el resultado de multiplicar por 2 es {resultado}')
