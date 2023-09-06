58f9ee6fad5d4da693ec19aad88d966c6e236643
Michelle Diaz <michellelunaby@gmail.com>
2023-09-06 01:14:16 -0600

    add metodo dir

--- metodos/metodos-lista.py
+++ metodos/metodos-lista.py
@@ -1,6 +1,9 @@
+#DIR  es una funcion de python que devuelve la lista de atributos validos del objeto pasado.
+#metodos que sirven con una lista: ['__add__', '__class__', '__class_getitem__', '__contains__', '__delattr__', '__delitem__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getstate__', '__gt__', '__hash__', '__iadd__', '__imul__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__reversed__', '__rmul__', '__setattr__', '__setitem__', '__sizeof__', '__str__', '__subclasshook__', 'append', 'clear', 'copy', 'count', 'extend', 'index', 'insert', 'pop', 'remove', 'reverse', 'sort']
+
 #LIST crea una lista
 lista = list(["hola", "Mich", 30])
-print (lista)
+print (dir(lista))
 
 #LEN  cuenta la cantidad de elementos de una lista
 resultado = len(lista)
