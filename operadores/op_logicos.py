b726af503dc95b3b1d531877cfe73d3925a8eb07
Michelle Diaz <michellelunaby@gmail.com>
2023-09-05 23:24:44 -0600

    operadores logicos

---
+++ operadores/op_logicos.py
@@ -0,0 +1,26 @@
+#AND
+Resultado = True & True #devolver True
+Resultado2 = False & True #devolver False
+Resultado3 = True & False #devolver False
+Resultado4 = False & False #devolver False
+
+#OR
+Resultado5 = True | True #devolver True
+Resultado6 = False | True #devolver True
+Resultado7 = True | False #devolver True
+Resultado8 = False | False #devolver False
+
+#NOT
+Resultado9 = not True  #devolver False
+Resultado10 = not False #devolver True
+
+print(Resultado)
+print(Resultado2)
+print(Resultado3)
+print(Resultado4)
+print(Resultado5)
+print(Resultado6)
+print(Resultado7)
+print(Resultado8)
+print(Resultado9)
+print(Resultado10)
