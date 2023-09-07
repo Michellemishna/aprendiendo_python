4e22c5c225fc6fd849aa56f70b455aad05037cf6
Michelle Diaz <michellelunaby@gmail.com>
2023-09-06 22:31:41 -0600

    iterar conjuntos

--- bucles/iterar_conjuntos.py
+++ bucles/iterar_conjuntos.py
@@ -1,6 +1,6 @@
 #el bucle for funciona con listas [] tuplas () y conjuntos {}
-animales = [ "gato", "perro", "loro", "cocodrilo", "pez" ]
-numeros = [52,32,16,72, 12]
+animales = { "gato", "perro", "loro", "cocodrilo", "pez" }
+numeros = {52,32,16,72, 12}
 
 for animal in animales: 
     print(f'Ahora la variable animal es: {animal}')
@@ -9,17 +9,17 @@ for numero in numeros:
     resultado = numero * 10
     print(resultado)
     
-#se pueden iterar dos o mas listas a la vez, solo es necesario que ambas tengan el mismo numero de elementos
+#se pueden iterar dos o mas conjuntos a la vez, solo es necesario que ambas tengan el mismo numero de elementos
 for numero,animal in zip(animales, numeros):
-    print(f'recorriendo lista 1: {numero}')
-    print(f'recorriendo lista 2: {animal}')
+    print(f'recorriendo conjunto 1: {numero}')
+    print(f'recorriendo conjunto 2: {animal}')
     
 #aca itera un rango de numeros iniciando con el primer numero dado y terminando antes de llegar al segundo parametro 
 #si le paso solo un parametro va a comenzar desde 0 y termina antes de llegar al numero dado
 for num in range(5,10):
     print(num)
     
-#forma correcta de recorrer una lista con su indice
+#forma correcta de recorrer una conjunto con su indice
 for num in enumerate(numeros):
     indice= num[0]
     valor =num[1]
