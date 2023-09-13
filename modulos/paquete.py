93f5b001f3675434d2e06447271bd013f1173f61
Michelle Diaz <michellelunaby@gmail.com>
2023-09-12 22:37:26 -0600

    paquetes

---
+++ modulos/paquete.py
@@ -0,0 +1,5 @@
+#en la carpeta que quiero que se aun paquete creo un archivo que se llame __init.py__ se crea un paquete y si hay un archivo igual dentro de una subcarpeta ocurre lo mismo
+
+
+import paquete
+print(paquete.__path__)
