6b9f000240b865dafefab57b6d68288d08ed9be8
Michelle Diaz <michellelunaby@gmail.com>
2023-09-12 22:19:15 -0600

    modulos

---
+++ modulos/_pycache_/modulos-2.0.py
@@ -0,0 +1,11 @@
+#si el modulo estuviera dentro de una carpeta en la misma ruta 
+#import funciones_buenas(nombre de la carpeta).saludar as m_saludar
+
+#python tiene modulos nativos, si se quiere crear alguno nuevo hay que tener cuidado de no crear uno con el mismo nombre
+#('_abc', '_ast', '_bisect', '_blake2', '_codecs', '_codecs_cn', '_codecs_hk', '_codecs_iso2022', '_codecs_jp', '_codecs_kr', '_codecs_tw', '_collections', '_contextvars', '_csv', '_datetime', '_functools', '_heapq', '_imp', '_io', '_json', '_locale', '_lsprof', '_md5', '_multibytecodec', '_opcode', '_operator', '_pickle', '_random', '_sha1', '_sha256', '_sha3', '_sha512', '_signal', '_sre', '_stat', '_statistics', '_string', '_struct', '_symtable', '_thread', '_tokenize', '_tracemalloc', '_typing', '_warnings', '_weakref', '_winapi', '_xxsubinterpreters', 'array', 'atexit', 'audioop', 'binascii', 'builtins', 'cmath', 'errno', 'faulthandler', 'gc', 'itertools', 'marshal', 'math', 'mmap', 'msvcrt', 'nt', 'sys', 'time', 'winreg', 'xxsubtype', 'zlib')
+import sys 
+print(sys.builtin_module_names)
+
+
+import sys
+print(sys.path)
